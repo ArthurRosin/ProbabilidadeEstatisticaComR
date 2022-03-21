5d7dd17d8b13218784b1a6cbcb4a0d7320eba671
ArthurRosin <arthur.rosin@aluno.ufabc.edu.br>
2022-03-21 17:50:22 -0300

    Primeiros comandos e operadores lógicos

---
+++ primeiroscomandos.R
@@ -0,0 +1,22 @@
+#Primeiros comandos e operadores lógicos
+
+raio <- 10
+altura <- 150
+espessura <- 1
+
+volume = pi * (raio-espessura)^2 * altura
+
+volume
+
+pi
+
+letters
+
+LETTERS
+
+x = 20
+y = 10
+
+y > y
+x > y
+y > x
