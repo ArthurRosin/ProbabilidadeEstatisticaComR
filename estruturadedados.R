99def6ddfcf7eae9dca11e4bf9f3a37414ef5be2
ArthurRosin <arthur.rosin@aluno.ufabc.edu.br>
2022-03-23 23:34:23 -0300

    Estrutura de dados

---
+++ estruturadedados.R
@@ -0,0 +1,23 @@
+#Estrutura de dados
+
+#vetores - valores númericos;lógicos;caracteres - nunca numérico e lógico juntos
+numerico <- c(1,2,3,4)
+numerico
+class(numerico)
+length(numerico)
+
+
+logico <- c(T, TRUE, FALSE, F)
+logico
+class(logico)
+length(logico)
+
+caracter <- c('a','aa',"aaa")
+caracter
+class(caracter)
+length(caracter)
+
+vetor <- c(numerico, logico, caracter)
+vetor
+class(vetor)
+length(vetor)
